7e969c7118f97d4272ea4bbdda7c049b8abb4d44
xuheng.zong <xuheng.zong2@dbappsecurity.com.cn>
2025-01-24 17:02:31 +0800

    refactor: 清理未使用的URL配置并调整设置
    1. 删除了未使用的admin URL配置。
    2. 将`DJANGO_DEBUG`环境变量从`false`改为`true`。
    3. 移除了不再需要的Django admin和auth相关的应用及中间件。

--- backend/porsche/settings.py
+++ backend/porsche/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
@@ -45,7 +43,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
