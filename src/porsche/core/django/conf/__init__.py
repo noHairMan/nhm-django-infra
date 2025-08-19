59f912b507f940a2464db285faf07c288cbc5f4a
noHairMan <79689669+nohairman@users.noreply.github.com>
2025-08-19 23:53:58 +0800

    fix: 修正 `Dynaconf` 配置中环境变量和设置文件路径解析方式
    - 替换 `django_settings` 依赖，改为直接使用 `os.environ` 获取环境变量；
    - 修正 `settings_files` 配置，将其动态解析为基于 `DJANGO_SETTINGS_MODULE` 的路径格式；
    - 确保 `Dynaconf` 配置兼容性及灵活性。

--- src/porsche/core/django/conf/__init__.py
+++ src/porsche/core/django/conf/__init__.py
@@ -1,9 +1,10 @@
-from django.conf import settings as django_settings
+import os
+
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
-    envvar_prefix=django_settings.APP,
-    settings_files=django_settings.SETTINGS_FILES,
+    envvar_prefix="DJANGO",
+    settings_files=[f"{"/".join(os.environ.get("DJANGO_SETTINGS_MODULE").split("."))}.py"],
     environments=True,
     load_dotenv=True,
 )
