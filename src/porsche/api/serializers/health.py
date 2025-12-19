b81ce8c2cae0f625e1deec59df7fb6951fde6ede
noHairMan <79689669+noHairMan@users.noreply.github.com>
2025-12-19 14:33:27 +0800

    feat: 添加调试信息字段到健康检查响应
    1. 在 `HealthCheckResponseSerializer` 中添加了 `debug` 字段，用于在响应中包含调试信息。
    2. 提升了健康检查的详细性，便于开发和运维人员排查问题。

--- src/porsche/api/serializers/health.py
+++ src/porsche/api/serializers/health.py
@@ -6,3 +6,4 @@ from porsche.core.restframework.serializer import PorscheSerializer
 class HealthCheckResponseSerializer(PorscheSerializer):
     app = serializers.CharField()
     version = serializers.CharField()
+    debug = serializers.BooleanField()
